7bf3d93b5dc661350ddd8d868291d5f31324a9e3
julian harley <julian.harley.fr@harpercollins.co.uk>
2019-12-08 20:46:18 +0000

    add static ROOT; helper function

--- tasks.py
+++ tasks.py
@@ -2,5 +2,5 @@ from invoke import task
 
 @task
 def deploy(c, docs=False, bytecode=False, extra=''):
-    c.run("heroku run python manage.py migrate --settings=sobfest.settings.production -a sobrietyfest")
-    c.run("heroku run python manage.py collectstatic --settings=sobfest.settings.production -a sobrietyfest")
+    c.run("heroku run python manage.py migrate --settings=ukrecoveryfest.settings.production -a sobrietyfest")
+    c.run("heroku run python manage.py collectstatic --settings=ukrecoveryfest.settings.production -a sobrietyfest")
